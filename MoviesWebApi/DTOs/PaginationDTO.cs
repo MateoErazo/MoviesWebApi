217681dbe1c8f8a0a574223a5ffa5611589fc306
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-04 12:29:22 -0500

    I add pagination

---
+++ MoviesWebApi/DTOs/PaginationDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace MoviesWebApi.DTOs
+{
+  public class PaginationDTO
+  {
+    private readonly int maximumRegistersByPage = 50;
+    private int pageSize = 10;
+    private int page = 1;
+    private readonly int minimumPage = 1;
+    public int Page {
+      get => page;
+      set
+      {
+        page = (value < minimumPage) ? minimumPage : value;
+      }
+    }
+    public int PageSize {
+      get => pageSize;
+      set
+      {
+        pageSize = (value > maximumRegistersByPage) ? maximumRegistersByPage : value;
+      }
+    }
+  }
+}
---
+++ MoviesWebApi/Helpers/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesWebApi.Helpers
+{
+  public static class HttpContextExtensions
+  {
+    public async static Task InsertParametersPagination<T>(this HttpContext httpContext,
+      IQueryable<T> queryable, int pageSize)
+    {
+      double amount = await queryable.CountAsync();
+      double pagesAmount = Math.Ceiling(amount / pageSize);
+      httpContext.Response.Headers.Add("pagesAmount", pagesAmount.ToString());
+    }
+  }
+}
