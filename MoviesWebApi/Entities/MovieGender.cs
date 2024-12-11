5ca57e605f259282d8b8b4b8e9f0f31b03c2d135
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-11 16:50:15 -0500

    I add relational data between entities

---
+++ MoviesWebApi/Entities/MovieGender.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesWebApi.Entities
+{
+  public class MovieGender
+  {
+    public int MovieId { get; set; }
+    public int GenderId { get; set; }
+    public Movie Movie { get; set; }
+    public Gender Gender { get; set; }
+
+  }
+}
--- MoviesWebApi/Migrations/20241211212909_MovieActor_MovieGender.Designer.cs
+++ MoviesWebApi/Migrations/20241211212909_MovieActor_MovieGender.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesWebApi;
 
@@ -11,9 +12,11 @@ using MoviesWebApi;
 namespace MoviesWebApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241211212909_MovieActor_MovieGender")]
+    partial class MovieActor_MovieGender
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
