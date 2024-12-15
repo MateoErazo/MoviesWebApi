dcb877f744bf56a2e4c7b55b401812398e9e1a6e
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-14 21:20:11 -0500

    I add relational data in movies controller

---
+++ MoviesWebApi/DTOs/ActorMovieDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesWebApi.DTOs
+{
+  public class ActorMovieDTO
+  {
+    public int Id { get; set; }
+    public string CharacterName { get; set; }
+    public string PersonName {  get; set; } 
+  }
+}
