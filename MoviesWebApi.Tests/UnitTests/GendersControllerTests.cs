580a1857c197fa273d00b6ebcc01cd55df053fc4
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-28 21:45:12 -0500

    I add get, create and update tests for genders controller

--- MoviesWebApi.Tests/UnitTests/GendersControllerTests.cs
+++ MoviesWebApi.Tests/UnitTests/GendersControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Controllers;
+using MoviesWebApi.DTOs;
+using MoviesWebApi.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +14,118 @@ namespace MoviesWebApi.Tests.UnitTests
   [TestClass]
   public class GendersControllerTests: TestsBase
   {
+    [TestMethod]
+    public async Task GetAllGenders()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+
+      dbContext.Genders.Add(new Gender { Name = "Gender 1"});
+      dbContext.Genders.Add(new Gender { Name = "Gender 2" });
+      await dbContext.SaveChangesAsync();
+
+      var dbContext2 = BuildDbContext(databaseName);
+
+      //test
+      var controller = new GendersController(mapper, dbContext2);
+      var response = await controller.GetAll();
+
+      //verification
+      var genders = response.Value;
+      Assert.AreEqual(2,genders.Count);
+    }
+
+    [TestMethod]
+    public async Task GetGenderByIdNotExist()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+      var controller = new GendersController(mapper,dbContext);
+
+      //test
+      var response = await controller.GetById(1);
+
+      //verification
+      var result = response.Result as StatusCodeResult;
+      Assert.AreEqual(404,result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task GetGenderByIdExisting()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+
+      dbContext.Add(new Gender() { Name = "Gender 1"});
+      await dbContext.SaveChangesAsync();
+      var dbContext2 = BuildDbContext(databaseName);
+      var controller = new GendersController(mapper, dbContext2);
+
+      //test
+      var response = await controller.GetById(1);
+
+      //verification
+      var result = response.Value;
+      Assert.AreEqual(1, result.Id);
+    }
+
+    [TestMethod]
+    public async Task CreateGender()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+      var controller = new GendersController(mapper, dbContext);
+      GenderCreationDTO genderDTO = new GenderCreationDTO() { Name = "Gender 1"};
+
+      //test
+      var response = await controller.Create(genderDTO);
+      var result = response as CreatedAtRouteResult;
+
+      //verification 1
+      Assert.IsNotNull(result);
+
+      //verification 2
+      var dbContext2 = BuildDbContext(databaseName);
+      var dbRegistersAmount = await dbContext2.Genders.CountAsync();
+      Assert.AreEqual(1, dbRegistersAmount);
+    }
+
+    [TestMethod]
+    public async Task UpdateGender()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+
+      dbContext.Genders.Add(new Gender() { Name = "Gender 1"});
+      await dbContext.SaveChangesAsync();
+
+      var dbContext2 = BuildDbContext(databaseName);
+      var controller = new GendersController(mapper, dbContext2);
+
+      int genderId = 1;
+      GenderCreationDTO genderDTO = new GenderCreationDTO() { Name = "Gender 1 Updated" };
+      
+      //test
+      var response = await controller.Update(genderId, genderDTO);
+      var result = response as StatusCodeResult;
+
+      //verification 1
+      Assert.AreEqual(204,result.StatusCode);
+
+      //verification 2
+      var dbContext3 = BuildDbContext(databaseName);
+      bool genderUpdatedExist = await dbContext3.Genders.AnyAsync(x => x.Name == "Gender 1 Updated");
+      Assert.IsTrue(genderUpdatedExist);
+    }
   }
 }
