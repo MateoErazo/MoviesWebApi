00ce273a1d68e0ddb18813635af1cbb42ab40847
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-29 17:38:17 -0500

    I add unit tests for actors controller

--- MoviesWebApi.Tests/UnitTests/GendersControllerTests.cs
+++ MoviesWebApi.Tests/UnitTests/GendersControllerTests.cs
@@ -127,5 +127,50 @@ namespace MoviesWebApi.Tests.UnitTests
       bool genderUpdatedExist = await dbContext3.Genders.AnyAsync(x => x.Name == "Gender 1 Updated");
       Assert.IsTrue(genderUpdatedExist);
     }
+
+    [TestMethod]
+    public async Task DeleteGenderByIdNotExisting()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+      var controller = new GendersController(mapper, dbContext);
+
+      //test
+      var response = await controller.Delete(1);
+      var result = response as StatusCodeResult;
+
+      //verification
+      Assert.AreEqual(404, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task DeleteGenderById()
+    {
+      //preparation
+      string databaseName = Guid.NewGuid().ToString();
+      var dbContext = BuildDbContext(databaseName);
+      var mapper = ConfigAutoMapper();
+
+      dbContext.Genders.Add(new Gender() { Name = "Gender 1"});
+      dbContext.Genders.Add(new Gender() { Name = "Gender 2" });
+      await dbContext.SaveChangesAsync();
+
+      var dbContext2 = BuildDbContext(databaseName);
+      var controller = new GendersController(mapper, dbContext2);
+
+      //test
+      var response = await controller.Delete(1);
+      var result = response as StatusCodeResult;
+
+      //verification 1
+      Assert.AreEqual(204, result.StatusCode);
+
+      //verification 2
+      var dbContext3 = BuildDbContext(databaseName);
+      var genders = await dbContext3.Genders.CountAsync();
+      Assert.AreEqual(1, genders);
+    }
   }
 }
