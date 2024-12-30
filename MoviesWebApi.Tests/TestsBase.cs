4e0eeed9788591f4c484ed5df715f6c81b4d8266
Jesús Mateo <erazojesusmateo@gmail.com>
2024-12-30 18:24:20 -0500

    I add unit tests for reviews controller

--- MoviesWebApi.Tests/TestsBase.cs
+++ MoviesWebApi.Tests/TestsBase.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesWebApi.Helpers;
 using NetTopologySuite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +15,8 @@ namespace MoviesWebApi.Tests
 {
   public class TestsBase
   {
+    protected readonly string defaultUserId = "5fe5f85a-f08e-41d5-8218-7e6ef94b9db6";
+    protected readonly string defaultUserEmail = "defaultuser@hotmail.com";
     protected ApplicationDbContext BuildDbContext(string databaseName)
     {
       var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -30,6 +35,21 @@ namespace MoviesWebApi.Tests
       });
 
       return config.CreateMapper();
+    }
+
+    protected ControllerContext BuildControllerContext()
+    {
+      var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+      {
+        new Claim(ClaimTypes.Name, defaultUserEmail),
+        new Claim(ClaimTypes.Email, defaultUserEmail),
+        new Claim(ClaimTypes.NameIdentifier, defaultUserId)
+      }));
+
+      return new ControllerContext()
+      {
+        HttpContext = new DefaultHttpContext() { User = user }
+      };
     }
   }
 }
