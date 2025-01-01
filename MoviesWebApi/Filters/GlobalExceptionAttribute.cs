24c636c0d099d0947269d60e951e957760c185e8
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-01 14:35:05 -0500

    I add global filter and update startup class

---
+++ MoviesWebApi/Filters/GlobalExceptionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MoviesWebApi.Filters
+{
+  public class GlobalExceptionAttribute : ExceptionFilterAttribute
+  {
+    private readonly ILogger<GlobalExceptionAttribute> logger;
+    public GlobalExceptionAttribute(ILogger<GlobalExceptionAttribute> logger)
+    {
+      this.logger = logger;
+    }
+
+    public override void OnException(ExceptionContext context)
+    {
+      logger.LogError(context.Exception, context.Exception.Message);
+      base.OnException(context);
+    }
+  }
+}
